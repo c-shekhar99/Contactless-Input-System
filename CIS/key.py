328ef5e081feb90dbe72274ce9c87894f15f3b7d
Chandra Shekhar Kushwaha <98191258+c-shekhar99@users.noreply.github.com>
2023-08-26 22:13:09 +0530

    Update key.py

--- CIS/key.py
+++ CIS/key.py
@@ -7,7 +7,7 @@ cap = cv2.VideoCapture(0)
 keyboard=Controller()
 keys=[["Q","W","E","R","T","Y","U","I","O","P"],
       ["A","S","D","F","G","H","J","K","L",";"],
-      ["Z","X","C","V","B","N","M",",",".","/"]]
+      ["Z","X","C","V","B","N","M",",",".","/"]      ]
 finalText=""
 def draw(img,buttonlist):
     for button in buttonlist:
